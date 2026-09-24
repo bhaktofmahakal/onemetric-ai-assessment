75e88382f83755f94a0e2b579e5dfdff44d060a3
utsav mishra <utsavmishraa005@gmail.com>
2026-09-24 12:40:51 +0530

    chore: add next.config.mjs and next-env.d.ts config files

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false,
+    appIsrStatus: false,
+  },
+  webpack: (config) => {
+    config.resolve.extensionAlias = {
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
